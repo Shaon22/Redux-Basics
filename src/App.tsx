c560377b38b86ee122da686b4c3bad1099148c3e
shaon22 <shaonroy11@gmail.com>
2025-06-16 00:22:01 +0600

    set up basic counter to implement redux

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>counter with redux</h1>
+      <div>0</div>
+      <button>Increament</button>
+      <button>Decreament</button>
+    </div>
+  )
+}
+
+export default App
